12bbf7cbc020cc5e88a866fbaf5cdfa9efba4d00
Gustavo <75509324+guskpo20@users.noreply.github.com>
2023-10-18 16:53:34 -0300

    Navbar based on figma

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -18,22 +18,42 @@ const Navbar = () => {
 
   return (
     <div className={`${styles.navbarcontainer}`}>
-        <div className={`${styles.logo}`}>
+        {/* <div className={`${styles.logo}`}>
             <Image src={Logo} width={50} height={50} alt='Logo de la marca'/>
-        </div>
+        </div> */}
         <div className={`${styles.navbar} ${navbarOpen ? styles.open : " "}`}>
-            <div>
-                <Link className={currentRoute == "/" ? styles.active : ""} href="/">Inicio</Link>
+            <div className={`${styles.mobileHide}`}>
+                <Link className={`${styles.name}`} href="/">Falta Uno</Link>
             </div>
             <div>
-                <Link className={currentRoute == "/search" ? styles.active : ""} href="/search">Buscar</Link>
+                <Link className={currentRoute == "/" ? styles.active : ""} href="/">Publicaciones</Link>
             </div>
             <div>
-                <Link className={currentRoute == "/fields" ? styles.active : ""} href="/fields">Canchas</Link>
+                <Link className={currentRoute == "/search" ? styles.active : ""} href="/search">Equipos</Link>
             </div>
             <div>
-                <Link className={currentRoute == "/about" ? styles.active : ""} href="/about">Sobre Nosotros</Link>
+                <Link className={currentRoute == "/fields" ? styles.active : ""} href="/fields">Jugadores</Link>
             </div>
+            <div>
+                <Link className={currentRoute == "/about" ? styles.active : ""} href="/about">Canchas</Link>
+            </div>
+            <div>
+                <Link className={`${styles.btn}`} href="/">INICIAR SESIÓN</Link>
+            </div>
+            <div>
+                <Link className={`${styles.btn}`} href="/">Registrarse</Link>
+            </div>
+        </div>
+        <div className={`${styles.btnsContainers}`}>
+            <div>
+                <Link className={`${styles.btn}`} href="/">INICIAR SESIÓN</Link>
+            </div>
+            <div>
+                <Link className={`${styles.btn}`} href="/">Registrarse</Link>
+            </div>
+        </div>
+        <div>
+            <Link className={`${styles.nameNavClose}`} href="/">Falta Uno</Link>
         </div>
         <div onClick={(e: any) => handleNavBtn(e)} className={`${styles.navbtn} ${ navbarOpen ? styles.open : "" }`}>
             <div className={`${styles.hamburguer}`}></div>
